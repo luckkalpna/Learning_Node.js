35b3ca9022fd17e90fc38cf00c658cffd27d646a
luckkalpna <luckkalpna@gmail.com>
2024-09-20 23:15:24 +0530

    If Else condition and for loop

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-console.log("Hello Server")
+// ------------Basic programm--------------
 
-var a = 10;
-var b = 20;
-var c = 30;
-console.log(a+b+c)
+// console.log("Hello Server")
+
+// var a = 10;
+// var b = 20;
+// var c = 30;
+// console.log(a+b+c)
+
+// --------------if else condition-------------
+
+// var x = "20";
+// if(x === 20){
+//     console.log("Matched")
+// }else{
+//     console.log("Not Matched")
+// }
+
+//---------For Loop in nodejs-------------
+
+var x = 10;
+for(i=0;i<10;i++){
+    console.log(i)
+}

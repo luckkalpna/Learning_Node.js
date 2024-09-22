19e7c5b69b4d249a1bf591ec86f5ca1047367536
luckkalpna <luckkalpna@gmail.com>
2024-09-22 20:21:37 +0530

    Create dynamically file and read in folder

--- index.js
+++ index.js
@@ -43,19 +43,19 @@
 
 //----------------Getting Input from command line---------------
 
-const fs = require("fs")
+// const fs = require("fs")
 
-const input = process.argv
+// const input = process.argv
 // fs.writeFileSync(input[2], input[3])
 
 //----------For adding and removing file-----------
 
-if(input[2]=='add'){
-    fs.writeFileSync(input[3], input[4])
-}
-else if(input[2]=='remove'){
-    fs.unlinkSync(input[3])
-}
-else{
-    console.log("Invalid Input")
-}
+// if(input[2]=='add'){
+//     fs.writeFileSync(input[3], input[4])
+// }
+// else if(input[2]=='remove'){
+//     fs.unlinkSync(input[3])
+// }
+// else{
+//     console.log("Invalid Input")
+// }
---
+++ path.js
@@ -0,0 +1,17 @@
+const fs = require("fs")
+
+const path = require("path");
+const dirPath = path.join(__dirname, 'files');
+// console.log(dirPath)
+
+//----------For create dynamically files-----------
+
+// for(i=0;i<5;i++){
+//     fs.writeFileSync(dirPath+"/hello"+i+".txt", "I am n demo content")
+// }
+
+//---------For read any folder how many files in there----------
+
+fs.readdir(dirPath, (err, files)=>{
+    console.log(files)
+})

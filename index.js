25e7c4629220da5bce9a9c66767ad0363ede7a0b
luckkalpna <luckkalpna@gmail.com>
2024-09-22 16:20:43 +0530

    File create and delete

--- index.js
+++ index.js
@@ -39,3 +39,23 @@
 
 // console.log("Nodemon Installed")
 
+
+
+//----------------Getting Input from command line---------------
+
+const fs = require("fs")
+
+const input = process.argv
+// fs.writeFileSync(input[2], input[3])
+
+//----------For adding and removing file-----------
+
+if(input[2]=='add'){
+    fs.writeFileSync(input[3], input[4])
+}
+else if(input[2]=='remove'){
+    fs.unlinkSync(input[3])
+}
+else{
+    console.log("Invalid Input")
+}

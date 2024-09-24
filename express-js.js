4f838f63eb4118be12741119eb966df0ab7ab655
luckkalpna <luckkalpna@gmail.com>
2024-09-24 23:16:27 +0530

    Create a basic HTML formatted website

--- express-js.js
+++ express-js.js
@@ -1,17 +1,29 @@
+// const express = require("express");
+// const app = express();
+
+// app.get("", (req, res)=>{
+//     res.send("Hello Server I am Home Page")
+// });
+
+// app.get("/about", (req, res)=>{
+//     res.send("Hello Server I am About page")
+// });
+
+// app.get("/help", (req, res)=>{
+//     res.send("Hello Server I am Help page")
+// });
+
+
+// app.listen(5000)
+
+//-----------Make a simple HTML page------------
+
 const express = require("express");
+const path = require("path");
+
 const app = express();
+const publicPath = path.join(__dirname, "public");
 
-app.get("", (req, res)=>{
-    res.send("Hello Server I am Home Page")
-});
+app.use(express.static(publicPath));
 
-app.get("/about", (req, res)=>{
-    res.send("Hello Server I am About page")
-});
-
-app.get("/help", (req, res)=>{
-    res.send("Hello Server I am Help page")
-});
-
-
-app.listen(5000)
+app.listen(5000);

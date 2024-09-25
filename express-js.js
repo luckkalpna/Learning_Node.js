ca1b1ebf9854c393e8deddd2cd1afa417f1bcc49
luckkalpna <luckkalpna@gmail.com>
2024-09-25 21:47:20 +0530

    remove extension from url

--- express-js.js
+++ express-js.js
@@ -24,6 +24,18 @@ const path = require("path");
 const app = express();
 const publicPath = path.join(__dirname, "public");
 
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
+
+app.get("", (req, res)=>{
+    res.sendFile(`${publicPath}/index.html`)
+});
+
+app.get("/about", (req, res)=>{
+    res.sendFile(`${publicPath}/about.html`)
+});
+
+app.get("/help", (req, res)=>{
+    res.sendFile(`${publicPath}/help.html`)
+})
 
 app.listen(5000);

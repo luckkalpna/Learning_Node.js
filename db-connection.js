ef73c6a1baeea272ea579a1f64eaa98161b08048
luckkalpna <luckkalpna@gmail.com>
2024-09-27 21:51:14 +0530

    MongoDB connect in a saperate file

---
+++ db-connection.js
@@ -0,0 +1,26 @@
+const {MongoClient} = require("mongodb");
+const url = "mongodb://localhost/27017";
+const client = new MongoClient(url);
+const database = "e-comm";
+
+// async function getData() {
+//     let result = await client.connect();
+//     let db = result.db(database);
+//     let collection = db.collection("products");
+//     let response = await collection.find({}).toArray();
+//     console.log(response)
+// }
+
+// getData();
+
+//----------Diffrent method-----------
+
+async function dbConnect() {
+    let result = await client.connect();
+    let db = result.db(database);
+    return db.collection("products");
+    // let response = await collection.find({}).toArray();
+    // console.log(response)
+}
+
+module.exports = dbConnect;
--- mongodb.js
+++ mongodb.js
@@ -1,34 +1,21 @@
-const {MongoClient} = require("mongodb");
-const url = "mongodb://localhost/27017";
-const client = new MongoClient(url);
-const database = "e-comm";
-
-// async function getData() {
-//     let result = await client.connect();
-//     let db = result.db(database);
-//     let collection = db.collection("products");
-//     let response = await collection.find({}).toArray();
-//     console.log(response)
-// }
-
-// getData();
-
-//----------Diffrent method-----------
-
-async function dbConnect() {
-    let result = await client.connect();
-    let db = result.db(database);
-    return db.collection("products");
-    // let response = await collection.find({}).toArray();
-    // console.log(response)
-}
+const dbConnect = require("./db-connection");
 
 //----------With the help of promisses----------
 
-dbConnect().then((res)=>{
-    res.find().toArray().then((data)=>{
-        console.log(data)
-    })
-})
+// dbConnect().then((res)=>{
+//     res.find().toArray().then((data)=>{
+//         console.log(data)
+//     })
+// })
+
+//-------------In a specific function-----------
+
+const main = async () =>{
+    let data = await dbConnect();
+    data = await data.find().toArray();
+    console.log(data)
+}
+
+main();
 
 // console.log(dbConnect());

e78ab76ccc6b2287faf9d516e6a422dc1ba367d1
luckkalpna <luckkalpna@gmail.com>
2024-09-27 21:37:19 +0530

    DataBase connect with promises

--- mongodb.js
+++ mongodb.js
@@ -3,12 +3,32 @@ const url = "mongodb://localhost/27017";
 const client = new MongoClient(url);
 const database = "e-comm";
 
-async function getData() {
+// async function getData() {
+//     let result = await client.connect();
+//     let db = result.db(database);
+//     let collection = db.collection("products");
+//     let response = await collection.find({}).toArray();
+//     console.log(response)
+// }
+
+// getData();
+
+//----------Diffrent method-----------
+
+async function dbConnect() {
     let result = await client.connect();
     let db = result.db(database);
-    let collection = db.collection("products");
-    let response = await collection.find({}).toArray();
-    console.log(response)
+    return db.collection("products");
+    // let response = await collection.find({}).toArray();
+    // console.log(response)
 }
 
-getData();
+//----------With the help of promisses----------
+
+dbConnect().then((res)=>{
+    res.find().toArray().then((data)=>{
+        console.log(data)
+    })
+})
+
+// console.log(dbConnect());

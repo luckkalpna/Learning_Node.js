a4d62e05c8a6582b088b29eaff33575308b3c4fe
luckkalpna <luckkalpna@gmail.com>
2024-09-28 14:57:27 +0530

    Create a basic GET API

--- delete-db.js
+++ delete-db.js
@@ -3,7 +3,7 @@ const dbConnect = require("./mongodb");
 const deleteData = async () =>{
     let data = await dbConnect()
     let result = await data.deleteOne({name:"m 9"});
-    console.log(result)
+    // console.log(result)
 }
 
 deleteData();

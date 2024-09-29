a05aee03816cfcab26b258968605e8f7550f8d16
luckkalpna <luckkalpna@gmail.com>
2024-09-29 19:56:18 +0530

    POST API with mongoose

---
+++ api-with-mongoose.js
@@ -0,0 +1,15 @@
+const express = require("express");
+require("./config");
+const Product = require("./product");
+
+const app =express();
+app.use(express.json());
+
+app.post("/create", async (req, res)=>{
+    let data = new Product(req.body);
+    let result = await data.save()
+    console.log(result)
+    res.send(result)
+})
+
+app.listen(5000);
